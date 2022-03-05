667593681bbb02330d112022636e55c4ee82bcc6
Murzin.V.S <murzinvit@ya.ru>
2022-03-05 15:16:54 +0500

    add kuber_deploy

---
+++ kuber_deploy/veriables.tf
@@ -0,0 +1,15 @@
+variable "token" {
+  default = ""
+}
+
+variable "cloud_id" {
+  default = "b1gthe1naa936dn511am"
+}
+
+variable "folder_id" {
+  default = "b1gogcvb32hqskgu5trk"
+}
+
+variable "zone" {
+  default = "ru-central1-a"
+}
